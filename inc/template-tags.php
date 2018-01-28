8a06141726b914d433c59c1a000ab69aa369ebf0
Antonio Larrosa <antonio.larrosa@gmail.com>
2018-01-28 13:18:20 +0100

    Post byline now uses images instead of text, add reading time, etc.
    Also, better support for mobile screens

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -23,20 +23,30 @@ if ( ! function_exists( 'ankh_morpork_posted_on' ) ) :
 			esc_attr( get_the_modified_date( 'c' ) ),
 			esc_html( get_the_modified_date() )
 		);
-
+/*
 		$posted_on = sprintf(
-			/* translators: %s: post date. */
+			/ * translators: %s: post date. * /
 			esc_html_x( 'Posted on %s', 'post date', 'ankh-morpork' ),
-			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+                        '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 		);
+ */
+                $posted_on = '<img src="' . get_template_directory_uri() . '/images/calendar.png" class="posted-on">' .
+                    '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>';
 
-		$byline = sprintf(
-			/* translators: %s: post author. */
-			esc_html_x( 'by %s', 'post author', 'ankh-morpork' ),
-			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-		);
+                $byline = '<img src="' . get_template_directory_uri() . '/images/user.png" class="byline">' .
+                    '<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>';
 
-		echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+                $rtime = do_shortcode('[rt_reading_time label="XX" postfix="XX" postfix_singular="XX"]');
+                $rtime = substr(strstr( $rtime, 'XX '), 3);
+                $rtime = strstr( $rtime, ' XX', true);
+
+                if ( intval($rtime) > 1 ) {
+		    $readtime = '<img src="' . get_template_directory_uri() . '/images/chronometer.png" class="readingtime">' . $rtime . ' mins.';
+                } else {
+		    $readtime = '<img src="' . get_template_directory_uri() . '/images/chronometer.png" class="readingtime">' . $rtime . ' min.';
+                }
+
+		echo '<span class="byline"> ' . $byline . '</span><span class="posted-on">' . $posted_on . '</span><span class="readingtime">' . $readtime . '</span>' ; // WPCS: XSS OK.
 
 	}
 endif;
