7e6b5c38b93253da1b6a934bc680324e005650cd
Antonio Larrosa <antonio.larrosa@gmail.com>
2018-07-10 20:18:54 +0200

    Use prism to add syntax highlightning to <code> elements

--- functions.php
+++ functions.php
@@ -125,6 +125,9 @@ function ankh_morpork_scripts() {
 
 	wp_enqueue_script( 'ankh-morpork-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
+	wp_enqueue_style( 'prism-style', get_template_directory_uri() . '/prism.css');
+	wp_enqueue_script( 'prism-script', get_template_directory_uri() . '/js/prism.js', array(), '1', true );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
